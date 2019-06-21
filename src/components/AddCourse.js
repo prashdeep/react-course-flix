a6c311040cdc4585eb6849a1ca49204f173e1526
pradeep <pradeep.kumar44@gmail.com>
2019-06-21 16:03:14 +0530

    addcourse

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -2,6 +2,44 @@ import React from 'react';
 
 class AddCourse extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state={
+            courseName:'',
+            coursePrice:'',
+            courseDuration:'',
+            invalidCourseName:''
+        };
+        this.handleCourseNameChange = this.handleCourseNameChange.bind(this);
+    }
+
+    handleCourseNameChange(event){
+        console.log('came inside the coursenamehange event');
+        const value = event.target.value;
+        console.log(value.length);
+        this.setState({
+            courseName:event.target.value
+        })
+        if ( value.length < 4){
+            console.log("------------------------------")
+            this.setState({
+                invalidCourseName:'Course length should be greater than 4 and less than 8'
+            })
+        }else {
+            this.setState({
+               invalidCourseName:'' 
+            })
+        }
+
+    }
+
+    handleSubmit(){
+        let course={};
+        course.name = this.state.courseName;
+
+        
+    }
+   
     render(){
         return (
             <div className="row">
@@ -9,18 +47,32 @@ class AddCourse extends React.Component {
                 <form>
                         <div className="form-group">
                             <label >Course Name</label>
-                            <input type="text" className="form-control" id="courseName" aria-describedby="courseName" placeholder="Enter Course Name"/>
+                            <input 
+                                name="courseName" 
+                                type="text" 
+                                value={this.state.courseName}
+                                className="form-control" 
+                                id="courseName" 
+                                aria-describedby="courseName" 
+                                placeholder="Enter Course Name"
+                                onChange={(event)=> this.handleCourseNameChange(event)}/>
+                        </div>
+                        <div>
+                            { this.state.invalidCourseName ? (<span className="alert alert-danger">{this.state.invalidCourseName}</span>)
+                                                           :''
+                            }
+                            
                         </div>
                         <div className="form-group">
                             <label >Course Price </label>
-                            <input type="text" class="form-control" id="price" placeholder="Price"/>
+                            <input name="coursePrice" type="text" className="form-control" id="price" placeholder="Price"/>
                         </div>
                         <div className="form-group">
                             <label >Course Duration </label>
-                            <input type="text" class="form-control" id="duration" placeholder="Price"/>
+                            <input type="text" name="courseDuration" className="form-control" id="duration" placeholder="Price"/>
                         </div>
                         
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary">Submit</button>
                     </form> 
                 </div>
             </div>
