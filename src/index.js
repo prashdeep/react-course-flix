7dfeec48542cffa8ec99b4f31b0ddaaf696263e2
pradeep <pradeep.kumar44@gmail.com>
2019-06-21 14:24:54 +0530

    committing the changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+import AddCourse from './components/AddCourse';
 
+ReactDOM.render(
+    <BrowserRouter>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+       <Route path="/" exact component={App}/>
+       <Route path="/add" exact component={AddCourse}/>
+
+       <Route path="/details" exact render={
+           ()=>{
+              return (<h1>This is Course details page</h1>)
+            }
+        }/>
+    </BrowserRouter>, 
+    
+    document.getElementById('root'));
 
